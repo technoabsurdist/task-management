28047f638e683550eb147a742f9fe59a08d8c296
Emi <andere@uchicago.edu>
2023-05-30 13:52:53 -0500

    added many-to-one one-to-many user <-> task

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -7,15 +7,20 @@ import {
     Patch, 
     Post, 
     Put, 
-    Query} 
+    Query,
+    UseGuards} 
 from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.entity';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { User } from 'src/auth/user.entity';
+import { GetUser } from 'src/auth/get-user.decorator';
 
 @Controller('tasks')
+@UseGuards(AuthGuard())
 export class TasksController {
     constructor(private tasksService: TasksService) { }
 
@@ -35,22 +40,20 @@ export class TasksController {
             return this.tasksService.deleteTaskById(id)
         }
 
-//     @Delete('/:id')
-//     deleteTaskById(@Param('id') id: string): void {
-//         return this.tasksService.deleteTaskById(id)
-//     }
+        @Patch('/:id/status')
+        updateTaskStatusById(
+            @Param('id') id: string,
+            @Body('') UpdateTaskStatusDto: UpdateTaskStatusDto 
+        ): Promise<Task> {
+            const { status } = UpdateTaskStatusDto
+            return this.tasksService.updateTaskStatusById(id, status)
+        }
 
-    @Patch('/:id/status')
-    updateTaskStatusById(
-        @Param('id') id: string,
-        @Body('') UpdateTaskStatusDto: UpdateTaskStatusDto 
-    ): Promise<Task> {
-        const { status } = UpdateTaskStatusDto
-        return this.tasksService.updateTaskStatusById(id, status)
-    }
-
-    @Post()
-    createTask(@Body() createTaskDto: CreateTaskDto): Promise<Task> {
-        return this.tasksService.createTask(createTaskDto)
-    }
+        @Post()
+        createTask(
+            @Body() createTaskDto: CreateTaskDto,
+            @GetUser() user: User
+        ): Promise<Task> {
+            return this.tasksService.createTask(createTaskDto, user)
+        }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -5,6 +5,7 @@ import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { TasksRepository } from './tasks.repository';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Task } from './task.entity';
+import { User } from 'src/auth/user.entity';
 
 @Injectable()
 export class TasksService {
@@ -42,8 +43,8 @@ export class TasksService {
         return task
     }
 
-    createTask(createTaskDto: CreateTaskDto): Promise<Task> {
-        return this.tasksRepository.createTask(createTaskDto)
+    createTask(createTaskDto: CreateTaskDto, user: User): Promise<Task> {
+        return this.tasksRepository.createTask(createTaskDto, user)
     }
     
 }
